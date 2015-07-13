f9ba6f863f6d13b15f134e8a6ece2b1712186aae
Brenna Pavlinchak <littlemeg27@fullsail.edu>
2015-07-12 23:08:27 -0400

    Almost finished

--- MGDProject1/MGDProject1/GameScene.m
+++ MGDProject1/MGDProject1/GameScene.m
@@ -8,23 +8,45 @@
 
 #import "GameScene.h"
 
+@interface GameScene ()
+
+@property (nonatomic) SKSpriteNode *brick1;
+@property (nonatomic) SKSpriteNode *brick2;
+@property (nonatomic) SKSpriteNode *brick3;
+@property (nonatomic) SKSpriteNode *brick4;
+
+@end
+
+static const uint32_t brickCategory   = 1; // 00000000000000000000000000000001
+static const uint32_t edgeCategory  = 2; // 00000000000000000000000000000010
+static const uint32_t ballCategory  = 4; // 00000000000000000000000000000100
+
 @implementation GameScene
 
--(void)didMoveToView:(SKView *)view
+-(id)initWithSize:(CGSize)size
 {
-    /* Setup your scene here */
-    SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
+    if (self = [super initWithSize:size])
+    {
+        /* Setup your scene here */
+        //SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
+        self.physicsBody = [SKPhysicsBody bodyWithEdgeLoopFromRect:self.frame];
+        self.physicsBody.categoryBitMask = edgeCategory;
+        self.physicsWorld.contactDelegate = self;
     
-    myLabel.text = @"Ball Bounce";
-    myLabel.fontSize = 20;
-    myLabel.position = CGPointMake(CGRectGetMidX(self.frame),
-                                   CGRectGetMidY(self.frame));
+        //myLabel.text = @"Ball Bounce";
+        //myLabel.fontSize = 20;
+        //myLabel.position = CGPointMake(CGRectGetMidX(self.frame),
+                                   //CGRectGetMidY(self.frame));
     
-    SKSpriteNode *bgImage = [SKSpriteNode spriteNodeWithImageNamed:@"featherBackground.jpg"];
-    bgImage.position = CGPointMake(self.size.width/2, self.size.height/2);
+        SKSpriteNode *background = [SKSpriteNode spriteNodeWithImageNamed:@"featherBackground.jpg"];
+        background.position = CGPointMake(self.size.width/2, self.size.height/2);
     
-    [self addChild:bgImage];
-    [self addChild:myLabel];
+        [self addChild:background];
+        //[self addChild:myLabel];
+        [self addBricks:size];
+        [self addBall:size];
+    }
+    return self;
 }
 
 -(void) addBall:(CGSize) size
@@ -34,55 +56,142 @@
     [self addChild:ball];
     
     ball.physicsBody = [SKPhysicsBody bodyWithTexture:ball.texture size:ball.texture.size];
-    CGPoint ballPoint = CGPointMake(160,200);
+    CGPoint ballPoint = CGPointMake(170,600);
     ball.position = ballPoint;
     
     ball.physicsBody.friction = 0;
     ball.physicsBody.linearDamping = 0;
     ball.physicsBody.restitution = 1.0f;
+    ball.physicsBody.categoryBitMask = brickCategory;
+    ball.physicsBody.contactTestBitMask = edgeCategory | brickCategory | ballCategory;
     
 }
 
--(void) addStarBricks:(CGSize) size
+-(void) addBricks:(CGSize) size
 {
-    SKSpriteNode *starBrick1 = [SKSpriteNode spriteNodeWithImageNamed:@"StarBrick1.png"];
-    SKSpriteNode *starBrick2 = [SKSpriteNode spriteNodeWithImageNamed:@"StarBrick2.png"];
-    SKSpriteNode *starBrick3 = [SKSpriteNode spriteNodeWithImageNamed:@"StarBrick3.png"];
-    SKSpriteNode *starBrick4 = [SKSpriteNode spriteNodeWithImageNamed:@"StarBrick4.png"];
-    SKSpriteNode *starBrick5 = [SKSpriteNode spriteNodeWithImageNamed:@"StarBrick5.png"];
-    SKSpriteNode *starBrick6 = [SKSpriteNode spriteNodeWithImageNamed:@"StarBrick6.png"];
+    self.brick1 = [SKSpriteNode spriteNodeWithImageNamed:@"brick1.png"];
+    self.brick2 = [SKSpriteNode spriteNodeWithImageNamed:@"brick2.png"];
+    self.brick3 = [SKSpriteNode spriteNodeWithImageNamed:@"brick3.png"];
+    self.brick4 = [SKSpriteNode spriteNodeWithImageNamed:@"brick4.png"];
     
-    [self addChild:starBrick1];
-    [self addChild:starBrick2];
-    [self addChild:starBrick3];
-    [self addChild:starBrick4];
-    [self addChild:starBrick5];
-    [self addChild:starBrick6];
+    self.brick1.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:self.brick1.frame.size];
+    self.brick1.physicsBody.dynamic =  NO;
+    CGPoint brickPoint1 = CGPointMake(115,350);
+    self.brick1.position = brickPoint1;
+    
+    self.brick2.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:self.brick2.frame.size];
+    self.brick2.physicsBody.dynamic =  NO;
+    CGPoint brickPoint2 = CGPointMake(300,250);
+    self.brick2.position = brickPoint2;
+    _brick2.physicsBody.categoryBitMask = brickCategory;
+    _brick2.physicsBody.contactTestBitMask = edgeCategory | brickCategory | ballCategory;
+    
+    self.brick3.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:self.brick3.frame.size];
+    self.brick3.physicsBody.dynamic =  NO;
+    CGPoint brickPoint3 = CGPointMake(135,150);
+    self.brick3.position = brickPoint3;
+    
+    self.brick4.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:self.brick4.frame.size];
+    self.brick4.physicsBody.dynamic =  NO;
+    CGPoint brickPoint4 = CGPointMake(250,450);
+    self.brick4.position = brickPoint4;
+    _brick4.physicsBody.categoryBitMask = brickCategory;
+    _brick4.physicsBody.contactTestBitMask = edgeCategory | brickCategory | ballCategory;
+    
+    [self addChild:self.brick1];
+    [self addChild:self.brick2];
+    [self addChild:self.brick3];
+    [self addChild:self.brick4];
+    
+    //Cant figure out how to make sure it runs and also does the hit
+    //Only have two of the bricks counting as bricks on purpose
     
 }
 
--(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
-    /* Called when a touch begins */
-    
-    /*for (UITouch *touch in touches)
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+     /* Called when a touch begins */
+     /*for (UITouch *touch in touches)
      {
-     CGPoint location = [touch locationInNode:self];
-     
-     SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
-     
-     sprite.xScale = 0.5;
-     sprite.yScale = 0.5;
-     sprite.position = location;
-     
-     SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
-     
-     [sprite runAction:[SKAction repeatActionForever:action]];
-     
-     [self addChild:sprite];
+
      }*/
 }
 
--(void)update:(CFTimeInterval)currentTime {
+-(void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    for (UITouch *touch in touches)
+    {
+        CGPoint location1 = [touch locationInNode:self];
+        CGPoint newBPos1 = CGPointMake(location1.x, 350);
+        
+        if (newBPos1.x < self.brick1.size.width/2)
+        {
+            newBPos1.x = self.brick1.size.width/2;
+        }
+        
+        if (newBPos1.x > self.size.width - (self.brick1.size.width/2))
+        {
+            newBPos1.x = self.size.width - (self.brick1.size.width/2);
+        }
+        
+        self.brick1.position = newBPos1;
+    }
+    
+    for (UITouch *touch in touches)
+    {
+        CGPoint location2 = [touch locationInNode:self];
+        CGPoint newBPos2 = CGPointMake(location2.x, 150);
+        
+        if (newBPos2.x < self.brick3.size.width/2)
+        {
+            newBPos2.x = self.brick3.size.width/2;
+        }
+        
+        if (newBPos2.x > self.size.width - (self.brick3.size.width/2))
+        {
+            newBPos2.x = self.size.width - (self.brick3.size.width/2);
+        }
+        
+        self.brick3.position = newBPos2;
+    } //Cant get them to move one at a time, found ideas on how to do, not enough time and also didnt know how to add in. Suggestions? 
+
+}
+
+-(void)didBeginContact:(SKPhysicsContact *)contact
+{
+    //Makes sound when the cards hit the wall
+    
+    SKPhysicsBody *notBall;
+    
+    if (contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask)
+    {
+        notBall = contact.bodyB;
+    }
+    else
+    {
+        notBall = contact.bodyA;
+    }
+    
+    if (notBall.categoryBitMask == edgeCategory)
+    {
+        SKAction *playSFX1 = [SKAction playSoundFileNamed:@"blip.caf" waitForCompletion:NO];
+        [self runAction:playSFX1];
+        NSLog(@"Bang!");
+    }
+    
+    if (notBall.categoryBitMask == brickCategory)
+    {
+        SKAction *playSFX1 = [SKAction playSoundFileNamed:@"brickhit.caf" waitForCompletion:NO];
+        [self runAction:playSFX1];
+        NSLog(@"Crash!");
+    }
+
+}
+
+
+
+-(void)update:(CFTimeInterval)currentTime
+{
     /* Called before each frame is rendered */
 }
 
