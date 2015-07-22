ad69cf929602d05463d88adcf3f4e6aedc64dd5c
Brenna Pavlinchak <littlemeg27@fullsail.edu>
2015-07-21 23:33:01 -0400

    Got the ball on the screen, not sure if tilt is working

--- MGDProject3/MGDProject3/GameScene.m
+++ MGDProject3/MGDProject3/GameScene.m
@@ -12,41 +12,21 @@
 
 @implementation GameScene
 
-    //FMMParallaxNode *_parallaxNodeBackgrounds;
-    //FMMParallaxNode *_parallaxSpaceDust;
-    
-    CMMotionManager *_motionManager;
-    
-    /*
-    NSMutableArray *_asteroids;
-    int _nextAsteroid;
-    double _nextAsteroidSpawn;
-    
-    NSMutableArray *_shipLasers;
-    int _nextShipLaser;
-    
-    int _lives;
-    double _gameOverTime;
-    bool _gameOver;
-    
-    AVAudioPlayer *_backgroundAudioPlayer;*/
-
+    CMMotionManager *motionManager; //Gets in the four types of motion
 
 -(id)initWithSize:(CGSize)size
 {
     if (self = [super initWithSize:size])
     {
         /* Setup your scene here */
+        self.backgroundColor = [SKColor brownColor];
         self.physicsBody = [SKPhysicsBody bodyWithEdgeLoopFromRect:self.frame];
         //self.physicsBody.categoryBitMask = edgeCategory;
         self.physicsWorld.contactDelegate = self; //Might need for later
+    
+        motionManager = [[CMMotionManager alloc] init];
         
-        SKSpriteNode *background = [SKSpriteNode spriteNodeWithImageNamed:@"featherBackground.jpg"];
-        background.position = CGPointMake(self.size.width/2, self.size.height/2);
-        
-        [self addChild:background];
         [self addBall:size];
-        [self startMovement];
     }
     return self;
 }
@@ -54,44 +34,12 @@
 -(void) addBall:(CGSize) size
 {
     SKSpriteNode *ball = [SKSpriteNode spriteNodeWithImageNamed:@"MainBall.png"];
+    [self addChild:ball];
 }
 
-- (void)startMovement
+
+-(void)update:(CFTimeInterval)currentTime
 {
-    ball.hidden = NO;
-    //reset ship position for new game
-    ball.position = CGPointMake(self.frame.size.width * 0.1, CGRectGetMidY(self.frame));
-    
-    //setup to handle accelerometer readings using CoreMotion Framework
-    [self startMonitoringAcceleration];
-    
-}
-
-- (void)startMonitoringAcceleration
-{
-    if (_motionManager.accelerometerAvailable) {
-        [_motionManager startAccelerometerUpdates];
-        NSLog(@"accelerometer updates on...");
-    }
-}
-
-- (void)stopMonitoringAcceleration
-{
-    if (_motionManager.accelerometerAvailable && _motionManager.accelerometerActive) {
-        [_motionManager stopAccelerometerUpdates];
-        NSLog(@"accelerometer updates off...");
-    }
-}
-
-- (void)updateShipPositionFromMotionManager
-{
-    CMAccelerometerData* data = _motionManager.accelerometerData;
-    if (fabs(data.acceleration.x) > 0.2) {
-        NSLog(@"acceleration value = %f",data.acceleration.x);
-    }
-}
-
--(void)update:(CFTimeInterval)currentTime {
     /* Called before each frame is rendered */
 }
 
