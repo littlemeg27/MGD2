d5986ba9bb7fdf8b40305393e18ca07852c7d2d5
Brenna Pavlinchak <littlemeg27@fullsail.edu>
2015-07-25 02:48:18 -0400

    Fixed ball

--- MGDProject3/MGDProject3/GameScene.m
+++ MGDProject3/MGDProject3/GameScene.m
@@ -10,7 +10,7 @@
 
 @interface GameScene ()
 
-@property (nonatomic) SKSpriteNode *ball;
+@property (nonatomic) SKShapeNode *ball;
 
 @end
 
@@ -42,7 +42,12 @@
 
 -(void) addBall:(CGSize) size
 {
-    self.ball = [SKSpriteNode spriteNodeWithImageNamed:@"MazeBall.png"];
+    float radius = 28;
+    self.ball = [SKShapeNode shapeNodeWithCircleOfRadius:radius];
+    CGPoint ballPoint = CGPointMake(size.width/2,size.height/2);
+    self.ball.position = ballPoint;
+    self.ball.fillColor = [SKColor redColor];
+    self.ball.strokeColor = [SKColor redColor];
     [self addChild:self.ball];
 }
 
@@ -50,61 +55,87 @@
 {
     SKSpriteNode *topWall = [SKSpriteNode spriteNodeWithColor:[SKColor blackColor] size:CGSizeMake(765, 8)];
     [topWall setPosition:CGPointMake(384, 950)];
+    topWall.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:topWall.frame.size];
+    topWall.physicsBody.dynamic =  NO;
     [self addChild:topWall];
     
     SKSpriteNode *middleLeft = [SKSpriteNode spriteNodeWithColor:[SKColor blackColor] size:CGSizeMake(435, 8)];
     [middleLeft setPosition:CGPointMake(55, 470)]; //Horizontal middle left line
+    middleLeft.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:middleLeft.frame.size];
+    middleLeft.physicsBody.dynamic =  NO;
     [self addChild:middleLeft];
     
     SKSpriteNode *bottomLeft = [SKSpriteNode spriteNodeWithColor:[SKColor blackColor] size:CGSizeMake(8, 350)];
     [bottomLeft setPosition:CGPointMake(140, 175)]; //Vertical bottom left line
+    bottomLeft.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:bottomLeft.frame.size];
+    bottomLeft.physicsBody.dynamic =  NO;
     [self addChild:bottomLeft];
     
     SKSpriteNode *topLeft = [SKSpriteNode spriteNodeWithColor:[SKColor blackColor] size:CGSizeMake(8, 350)];
     [topLeft setPosition:CGPointMake(140, 775)]; //Vertical top left line
+    topLeft.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:topLeft.frame.size];
+    topLeft.physicsBody.dynamic =  NO;
     [self addChild:topLeft];
     
     SKSpriteNode *bottomRight = [SKSpriteNode spriteNodeWithColor:[SKColor blackColor] size:CGSizeMake(8, 700)];
     [bottomRight setPosition:CGPointMake(630, 150)]; //Vertical bottom right line
+    bottomRight.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:bottomRight.frame.size];
+    bottomRight.physicsBody.dynamic =  NO;
     [self addChild:bottomRight];
     
     SKSpriteNode *bottomMiddle2 = [SKSpriteNode spriteNodeWithColor:[SKColor blackColor] size:CGSizeMake(330, 8)];
     [bottomMiddle2 setPosition:CGPointMake(305, 230)]; //Horizontal middle bottom 2 line
+    bottomMiddle2.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:bottomMiddle2.frame.size];
+    bottomMiddle2.physicsBody.dynamic =  NO;
     [self addChild:bottomMiddle2];
     
     SKSpriteNode *bottomMiddle1 = [SKSpriteNode spriteNodeWithColor:[SKColor blackColor] size:CGSizeMake(330, 8)];
     [bottomMiddle1 setPosition:CGPointMake(465, 110)]; //Horizontal middle bottom 1 line
+    bottomMiddle1.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:bottomMiddle1.frame.size];
+    bottomMiddle1.physicsBody.dynamic =  NO;
     [self addChild:bottomMiddle1];
     
     SKSpriteNode *leftMiddle = [SKSpriteNode spriteNodeWithColor:[SKColor blackColor] size:CGSizeMake(8, 500)];
     [leftMiddle setPosition:CGPointMake(270, 600)]; //Vertical middle left line
+    leftMiddle.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:leftMiddle.frame.size];
+    leftMiddle.physicsBody.dynamic =  NO;
     [self addChild:leftMiddle];
     
     SKSpriteNode *topMiddle1 = [SKSpriteNode spriteNodeWithColor:[SKColor blackColor] size:CGSizeMake(380, 8)];
     [topMiddle1 setPosition:CGPointMake(456, 847)]; //Horizontal top middle line
+    topMiddle1.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:topMiddle1.frame.size];
+    topMiddle1.physicsBody.dynamic =  NO;
     [self addChild:topMiddle1];
     
     SKSpriteNode *topMiddle2 = [SKSpriteNode spriteNodeWithColor:[SKColor blackColor] size:CGSizeMake(8, 250)];
     [topMiddle2 setPosition:CGPointMake(385, 620)]; //Vertical top middle line
+    topMiddle2.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:topMiddle2.frame.size];
+    topMiddle2.physicsBody.dynamic =  NO;
     [self addChild:topMiddle2];
     
     SKSpriteNode *middleRight1 = [SKSpriteNode spriteNodeWithColor:[SKColor blackColor] size:CGSizeMake(250, 8)];
     [middleRight1 setPosition:CGPointMake(506, 745)]; //Horizontal middle right line
+    middleRight1.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:middleRight1.frame.size];
+    middleRight1.physicsBody.dynamic =  NO;
     [self addChild:middleRight1];
     
     SKSpriteNode *middleRight2 = [SKSpriteNode spriteNodeWithColor:[SKColor blackColor] size:CGSizeMake(250, 8)];
     [middleRight2 setPosition:CGPointMake(505, 496)]; //Horizontal middle lower right line
+    middleRight2.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:middleRight2.frame.size];
+    middleRight2.physicsBody.dynamic =  NO;
     [self addChild:middleRight2];
     
     SKSpriteNode *rightMiddle = [SKSpriteNode spriteNodeWithColor:[SKColor blackColor] size:CGSizeMake(250, 8)];
     [rightMiddle setPosition:CGPointMake(645, 620)]; //Horizontal middle lower right line
+    rightMiddle.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:rightMiddle.frame.size];
+    rightMiddle.physicsBody.dynamic =  NO;
     [self addChild:rightMiddle];
     
     SKSpriteNode *bottomMiddle3 = [SKSpriteNode spriteNodeWithColor:[SKColor blackColor] size:CGSizeMake(250, 8)];
     [bottomMiddle3 setPosition:CGPointMake(391, 350)]; //Horizontal right middle line
+    bottomMiddle3.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:bottomMiddle3.frame.size];
+    bottomMiddle3.physicsBody.dynamic =  NO;
     [self addChild:bottomMiddle3];
-    
-
 }
 
 
@@ -118,12 +149,12 @@
 - (void)startGame
 {
     self.ball.hidden = NO;   //reset ball position for new game
-    self.ball.physicsBody = [SKPhysicsBody bodyWithTexture:self.ball.texture size:self.ball.texture.size];
-    CGPoint ballPoint = CGPointMake(170,400);
+    self.ball.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:self.ball.frame.size.width/2];
+    CGPoint ballPoint = CGPointMake(75,875);
     self.ball.position = ballPoint; 
     self.ball.physicsBody.dynamic = YES; //Not sitting still
     self.ball.physicsBody.affectedByGravity = NO; //Not affected
-    self.ball.physicsBody.mass = 0.02; //Give mass
+    self.ball.physicsBody.mass = 0.05; //Give mass
     
     //setup to handle accelerometer readings using CoreMotion Framework
     [self startAccelerationOn]; //Yeah not sure
